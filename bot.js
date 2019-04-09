e635e864e00a455e28dadae06173f398aa2c94c3
Kokiwi <kleiba@student.42.fr>
2019-04-09 15:14:12 +0200

    tacobot now stops talking. He just adds a reaction

--- bot.js
+++ bot.js
@@ -11,20 +11,21 @@ const forTaco = controller => {
       const senderIndex = ids.indexOf(message.user);
       const tacosGiven = parser.countTacos(message.text);
       if (userIndex > -1 && userIndex !== senderIndex) {
-        const user = DB.getUser(userIndex);
         const giver = DB.getUser(senderIndex);
         if (giver.left >= tacosGiven) {
           taco.giveTaco(userIndex, tacosGiven);
           taco.removeLeft(senderIndex, tacosGiven);
-          bot.reply(
-            message,
-            `Congratz ! <@${user.id}> now has ${user.tacos +
-              tacosGiven} tacos !`
-          );
-          bot.reply(
-            message,
-            `<@${message.user}> now has ${giver.left -
-              tacosGiven} tacos left... Nice job guys`
+          bot.api.reactions.add(
+            {
+              timestamp: message.ts,
+              channel: message.channel,
+              name: "taco"
+            },
+            function(err, res) {
+              if (err) {
+                bot.botkit.log("Failed to add emoji reaction :(", err);
+              }
+            }
           );
         } else {
           bot.reply(
